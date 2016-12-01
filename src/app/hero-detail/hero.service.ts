6757fe1c54422e366e61d6675522f82c548f1d46
Yurik Abdulayev <yuriia@softwareplanet.com>
2016-12-01 19:56:53 +0200

    need include proxy some where

--- src/app/hero-detail/hero.service.ts
+++ src/app/hero-detail/hero.service.ts
@@ -1,12 +1,23 @@
 import {Injectable} from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  // getHeroes(): Promise<Hero[]> {
+  //   return Promise.resolve(HEROES);
+  // }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    heroesUrl = 'api/heroes';  // URL to web api
+
+    return this.http.get('api/heroes')
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
